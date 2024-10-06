8d1a891a0fabe79c886eac4f8f390c0db97fe8b1
htppsvitor <vitor.qn2004@gmail.com>
2024-10-06 13:32:03 -0300

    PMC

---
+++ Models/ExampleException.cs
@@ -0,0 +1,33 @@
+﻿namespace PMC;
+
+public class ExampleException
+{
+
+    public void Method1()
+    {
+        Method2();
+    }
+
+    public void Method2()
+    {
+        try{
+            Method3();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Excepition fix:" + ex.StackTrace);
+        }
+    }
+
+    public void Method3()
+    {
+        Method4();
+    }
+
+    public void Method4()
+    {
+        throw new Exception("Occurred a exception");
+    }
+
+}
+
--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using ModelCourse;
 using System.Security.Cryptography;
 using PMC;
 
-
+//  DICTIONARYS
 
 Dictionary<string, string> states = new Dictionary<string, string>
 ();
@@ -53,103 +53,87 @@ foreach(KeyValuePair<string, string> item in states)
 
 
 
+// STACK
+
+Console.WriteLine("-----Stack-----");
+
+Stack<int> stack = new Stack<int>();
+
+stack.Push(1);
+stack.Push(4);
+stack.Push(3);
+stack.Push(2);
+
+foreach(int item in stack)
+{
+    Console.WriteLine(item);
+}
+
+stack.Pop();
+
+Console.WriteLine("----------");
+foreach(int item in stack)
+{
+    Console.WriteLine(item);
+}
 
 
 
+// QUEUE
+Console.WriteLine("-----Queue-----");
+
+Queue<int> queue = new Queue<int>();
+
+queue.Enqueue(1);
+queue.Enqueue(4);
+queue.Enqueue(3);
+queue.Enqueue(2);
+
+foreach(int item in queue)
+{
+    Console.WriteLine(item);
+}
+
+queue.Dequeue();
+
+Console.WriteLine("----------");
+foreach(int item in queue)
+{
+    Console.WriteLine(item);
+}
+
+new ExampleException().Method1();
 
 
+//EXCEPTIONS
 
+try // If occurred a wrong, your program doesn't stop and takes care of that error
+{
 
+    string[] lines = File.ReadAllLines("Folders/FileRead.txt");
 
+    foreach(string line in lines)
+    {
+        Console.WriteLine(line);
+    }
 
-
-
-
-
-
-
-
-// // STACK
-
-// Console.WriteLine("-----Stack-----");
-
-// Stack<int> stack = new Stack<int>();
-
-// stack.Push(1);
-// stack.Push(4);
-// stack.Push(3);
-// stack.Push(2);
-
-// foreach(int item in stack)
-// {
-//     Console.WriteLine(item);
-// }
-
-// stack.Pop();
-
-// Console.WriteLine("----------");
-// foreach(int item in stack)
-// {
-//     Console.WriteLine(item);
-// }
-
-
-
-// // QUEUE
-// Console.WriteLine("-----Queue-----");
-
-// Queue<int> queue = new Queue<int>();
-
-// queue.Enqueue(1);
-// queue.Enqueue(4);
-// queue.Enqueue(3);
-// queue.Enqueue(2);
-
-// foreach(int item in queue)
-// {
-//     Console.WriteLine(item);
-// }
-
-// queue.Dequeue();
-
-// Console.WriteLine("----------");
-// foreach(int item in queue)
-// {
-//     Console.WriteLine(item);
-// }
-
-// new ExampleException().Method1();
-
-
-
-
-// try // If occurred a wrong, your program doesn't stop and takes care of that error
-// {
-
-//     string[] lines = File.ReadAllLines("Folders/FileRead.txt");
-
-//     foreach(string line in lines)
-//     {
-//         Console.WriteLine(line);
-//     }
-
-// } 
-// catch(FileNotFoundException ex) 
-// {
-//     Console.WriteLine($"An error occurred while reading the file. File doesn't found. {ex.Message}");
-// }
-// catch(DirectoryNotFoundException ex) 
-// {
-//     Console.WriteLine($"An error occurred while reading the file. Directory doesn't found. {ex.Message}");
-// }
-// catch(Exception ex)
-// {
-//     Console.WriteLine($"A generic exception occurred: {ex.Message}");
-// }
-// finally
-// {
-//     Console.WriteLine($"IT GOT HERE!!!");
-// }
+} 
+catch(FileNotFoundException ex) 
+{
+    Console.WriteLine($"An error occurred while reading the file. File doesn't found. {ex.Message}");
+}
+catch(DirectoryNotFoundException ex) 
+{
+    Console.WriteLine($"An error occurred while reading the file. Directory doesn't found. {ex.Message}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"A generic exception occurred: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine($"IT GOT HERE!!!");
+}
 
 
 
